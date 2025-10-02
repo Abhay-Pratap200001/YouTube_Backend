88c6acc24db48fccf7d4b47f43755c0b3e894fd5
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-10-02 11:02:44 +0530

    Adding classes amd define higher order function for error and response

---
+++ src/utils/asynHandler.js
@@ -0,0 +1,8 @@
+//For detacting the error
+const asynHandler = (requestHandler) => {
+ return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asynHandler };
