2451e8430830a574d99795ad3bdc2ad1e89fbe3c
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-10-02 11:01:23 +0530

    connecting server into db

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import dotenv from "dotenv";
-dotenv.config();
 import { connectDB } from "./db/dbConnect.js";
+import { app } from "./app.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 2000;
 
 connectDB()
+  .then(() => {
+    const server = app.listen(PORT, () => {
+      console.log(`✅ Server running at http://localhost:${PORT}`);
+    });
+
+    // server error handling
+    server.on("error", (error) => {
+      console.error("❌ Server Error:", error);
+      process.exit(1); // Exit the app if server fails
+    });
+  })
+  .catch((error) => {
+    console.error("❌ MongoDB Connection Failed:", error);
+    process.exit(1); // Exit app if DB connection fails
+  });
