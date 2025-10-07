e8679e3011c606882160eb7ef722270ae6fdb949
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-10-07 17:12:41 +0530

    Add verifyJwt middleware in logout route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logOutUser, registerUser,} from "../controllers/user.controller.js";
 import {upload} from '../middlewares/multer.middleware.js'
+import { verifyJwt } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
 router.route('/register').post( upload.fields([{name: 'avatar', maxCount: 1}, {name: 'coverImage',   maxCount: 1}]), registerUser)
+router.route('/login').post(loginUser)
+router.route('/logout').post(verifyJwt, logOutUser)
 
 
 export default router
