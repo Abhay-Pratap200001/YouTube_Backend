c42f9cc8e441db0bd44f0bb5410a0c6f75ca6e3b
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-10-08 10:12:48 +0530

    fix some type errors

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,21 +1,21 @@
 import  jwt  from "jsonwebtoken";
-import { ApiError } from "../utils/ApiError";
-import { asynHandler } from "../utils/asynHandler";
-import { User } from "../models/user.model";
+import { ApiError } from "../utils/ApiError.js";
+import { asynHandler } from "../utils/asynHandler.js";
+import { User } from "../models/user.model.js";
 
 export const verifyJwt = asynHandler(async(req, _, next) =>{
    try {
-    const token = req.cookie?.accessToken || req.header('Authorization')?.replace('Bearer ', '')
+    const token = req.cookies?.accessToken || req.header('Authorization')?.replace('Bearer ', '')
     if (!token) {
      throw new ApiError(401, 'Unauthorized request')
     }
  
-    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRECT)
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
     const user = await User.findById(decodedToken?._id).select('-password -refreshToken')
     if (!user) {
      throw new ApiError(401, 'Invalid Acess Token')
     }
- 
+
     req.user = user
     next()
    } catch (error) {
