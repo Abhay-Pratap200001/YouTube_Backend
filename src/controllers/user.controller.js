2d0d2c7d42f9242a9b1139e03f44b83224e54da7
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-10-14 18:58:23 +0530

    Write aggregation pipeline for user subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -194,7 +194,7 @@ export const changeCurrentPassword = asynHandler(async(req, res)=>{
 
 //Getting current user or user profile detail
  export const getCurrentUser = asynHandler(async(req, res) => {
-    return res.status(200).json(200, req.user, 'Current User get successfully')
+    return res.status(200).json(new ApiResponse (200, req.user, 'Current User get successfully'))
 })
 
 
@@ -247,4 +247,79 @@ export const updateUserCover =  asynHandler(async(req, res)=>{
   const user =  await User.findByIdAndUpdate(req.user?._id, {$set:{coverImage: coverImage.url}}, {new: true}).select('-password')
   return res.status(200).json(new ApiResponse(200, user, 'Cover image update'))
 })
-//
+
+
+
+
+export const getUserChannelProfile = asynHandler(async(req, res) =>{
+  const {username} = req.params
+  if (!username?.trim()) {
+    throw new ApiError(400, 'username is missing')
+  }
+
+   const channel = await User.aggregate([{
+    $match:{
+      username: username?.toLowerCase()
+    }
+   },
+   
+   {
+    $lookup:{
+      from:'subscriptions',
+      localField:"_id",
+      foreignField:"channel",
+      as:'subscribers'
+    }
+   },
+
+   {
+      $lookup:{
+      from:'subscriptions',
+      localField:"_id",
+      foreignField:"subscriber",
+      as:'subscriberdTo'
+    }  
+   },
+
+   {
+    $addFields:{
+      subscribersCount: {
+        $size: '$subscribers'
+      },
+
+      channelsSubscribedToCount: {
+        $size:'$subscriberdTo'
+      },
+      
+      isSubscribed:{
+        $cond:{
+          if: { $in: [req.user?._id, "$subscribers.subscriber"]},
+          then: true,
+          else: false
+        }
+      }
+    }
+   },
+   
+   {
+    $project: {
+      fullname: 1,
+      username: 1,
+      subscribersCount: 1,
+      channelsSubscribedToCount: 1,
+      isSubscribed: 1,
+      avatar: 1,
+      coverImage:1,
+      email: 1 
+    }
+   }
+
+  ])
+
+  if (!channel?.length) {
+    throw new ApiError(400, 'channel does not exist')
+  }
+
+  return res.status(200).json(new ApiResponse(200, channel[0], 'User channel fetched successfully'))
+})
+ 
